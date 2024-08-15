598dfac148c620c4616f139c2fc578deb24cbbbd
Dm&try <dmitrypanyukov@gmail.com>
2024-08-15 14:50:54 +0300

    Step0

---
+++ src/main/java/com/vdjOlnColectionMnogoobrRelis/vdjOlnColectionMnogoobrRelisspringdemo/VdjOlnColectionMnogoobrRelisSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vdjOlnColectionMnogoobrRelis.vdjOlnColectionMnogoobrRelisspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdjOlnColectionMnogoobrRelisSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdjOlnColectionMnogoobrRelisSpringDemoApplication.class, args);
+	}
+
+}
